84380d2a278985125f263734df93e513438e05c0
richardccf <richard@carboncopies.org>
2025-09-09 11:57:25 -0500

    deployment errs

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/study-tool-leetcode/',
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
